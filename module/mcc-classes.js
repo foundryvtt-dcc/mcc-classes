9185b6fee51ef219c9f47e50baac337f3d24f41d
Christian Ovsenik <ceovsenik@gmail.com>
2020-10-23 18:26:37 -0400

    merge classes into mcc-classes.js

--- module/mcc-classes.js
+++ module/mcc-classes.js
@@ -1,25 +1,25 @@
 /* global Actors */
 
-import * as healerSheets from './actor-sheets-healer.js'
-import * as MutantSheets from './actor-sheets-mutant.js'
-import * as RoverSheets from './actor-sheets-rover.js'
-import * as SentinelSheets from './actor-sheets-sentinel.js'
-import * as shamanSheets from './actor-sheets-shaman.js'
-
+import * as HealerSheets from './actor-sheets-healer.js';
+import * as MutantSheets from './actor-sheets-mutant.js';
+import * as RoverSheets from './actor-sheets-rover.js';
+import * as SentinelSheets from './actor-sheets-sentinel.js';
+import * as ShamanSheets from './actor-sheets-shaman.js';
 
 /* -------------------------------------------- */
 /*  Foundry VTT Initialization                  */
 /* -------------------------------------------- */
+
 Hooks.once('init', async function () {
   console.log(`DCC | Initializing Dungeon Crawl Classics System`)
 
   // Register sheet application classes
-  Actors.registerSheet('mcc-healer', healerSheets.ActorSheetHealer)
-  Actors.registerSheet('mcc-healer', healerSheets.ActorSheetHealer)
+  Actors.registerSheet('mcc-healer', HealerSheets.ActorSheetHealer)
   Actors.registerSheet('mcc-mutant', MutantSheets.ActorSheetMutant)
   Actors.registerSheet('mcc-rover', RoverSheets.ActorSheetRover)
   Actors.registerSheet('mcc-sentinel', SentinelSheets.ActorSheetSentinel)
-  Actors.registerSheet('mcc-shaman', shamanSheets.ActorSheetShaman)  
+  Actors.registerSheet('mcc-shaman', ShamanSheets.ActorSheetShaman)  
+  
   // Register shared template for MCC characters
   const templatePaths = [
 	'modules/mcc-classes/templates/actor-partial-pc-mcc-header.html',
