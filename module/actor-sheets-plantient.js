1b6f11a5b75e0ef7bca188e4b0efb2c09ab371f0
Christian Ovsenik <ceovsenik@gmail.com>
2022-11-07 13:11:30 -0500

    Fix for v10

--- module/actor-sheets-plantient.js
+++ module/actor-sheets-plantient.js
@@ -12,6 +12,8 @@ class ActorSheetPlantient extends DCCActorSheet {
   /** @override */
 /**  getData () {
     const data = super.getData() **/
+async getData (options) {
+    const data = await super.getData(options)
     this.options.template = 'modules/mcc-classes/templates/actor-sheet-plantient.html'
     if (data.system.details.sheetClass !== 'Plantient') {
       this.actor.update({
